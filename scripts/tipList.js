ce2a15992eef81714f1a43e650840515386b4255
Gavin Beaudet <gman.beaudet@gmail.com>
2025-05-21 12:49:36 -0400

    Wrap tips in a bulleted list using ul and li

--- scripts/tipList.js
+++ scripts/tipList.js
@@ -1,19 +1,22 @@
 import { database } from "./aquariumData.js";
 
 export const tipList = () => {
-  // Generate an HTML representation of each tip
-  let tipHTML = "";
+  // Generate an HTML representation of each tip as a bulleted list of articles
+  let tipHTML = "<h2>Tips</h2><ul>";
 
   for (const tip of database.tips) {
     tipHTML += `
-           <article class="tip">
-               <div class="tip_details">
-                   <h2 class="tip_topic">${tip.topic}</h2>
-                   <p class="tip_text">${tip.text}</p>
-               </div>
-           </article>
-        `;
+      <li>
+        <article class="tip">
+          <div class="tip_details">
+            <h2 class="tip_topic">${tip.topic}</h2>
+            <p class="tip_text">${tip.text}</p>
+          </div>
+        </article>
+      </li>
+    `;
   }
 
+  tipHTML += "</ul>";
   return tipHTML;
 };
